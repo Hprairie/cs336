c23f0a06f3d0a6d3306c7f4a30d7c3d5cdf4a2d7
hprairie <hprairie17@gmail.com>
2025-06-28 14:48:15 -0500

    finished generator

--- assignment1-basics/cs336_basics/generate.py
+++ assignment1-basics/cs336_basics/generate.py
@@ -13,9 +13,14 @@ class Generator:
     def _generate_single_token(self, input_ids: list[int], temperature: float, threshold: float) -> int:
         logits = self.model(torch.Tensor(input_ids).to(next(self.model.parameters()).device))
         probs = softmax(logits[-1, :], temperature=temperature)
-
         # Truncate to top p
         sorted, indexs = torch.sort(probs, dim=-1)
+        cs_sorted = torch.cumsum(sorted, dim=-1)
+        mask = cs_sorted < threshold
+        probs[indexs[~mask]] = 0
+        # Sample from distribution
+        out_id = torch.multinomial(probs, num_samples=1)
+        return int(out_id.item())
     
     def generate(self, input: str, max_generated_tokens: int | None = None, temperature: float = 0.9, threshold: float = 0.1) -> str:
         input_ids = self.tokenizer.encode(input)
